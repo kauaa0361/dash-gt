3aa4100ae580a425f27a22804cec83bde4e554aa
kauaa0361 <kauacosta1111@gmail.com>
2023-03-06 15:20:18 +0000

    update dashboard

--- components/dashboard/index.jsx
+++ components/dashboard/index.jsx
@@ -1,8 +1,14 @@
+import { useState,useEffect } from "react";
 import Image from "next/image"
 
-export function CustomPage() {
-  const a = require("/apiteste.json")
-  console.log(a);
+export default function Dashboard() {
+
+  const [download, setDownload] = useState(false)
+
+  useEffect(() => {
+    setDownload(true);
+  }, []);
+
   return(
     <main className="ml-10 mr-10">
       <Title icon="/ads.jpg" title="Visao Geral Google Ads"/>
@@ -25,7 +31,7 @@ function DataSection({title, value}) {
     <section className="uppercase w-40 h-20 mt-5">
       <p className="text-md font-light text-slate-700">{title}</p>
       <p className="text-3xl font-medium mt-4">{value}</p>
-      <div className="bg-slate-200 w-full h-2 mt-2 rounded-md"/>
+      <div className="bg-gray-500 w-full h-2 mt-2 rounded-md"/>
     </section>
   )
 }
@@ -35,18 +41,19 @@ function Title({icon,title}) {
     <section className="flex mt-10">
       <Image className="w-8 h-8" width={200} height={200} src={icon} alt="ads logo"/>
       <h1 className="text-2xl font-medium">{title}</h1>
-  </section>
+    </section>
   )
 }
 
 function CustomContent() {
   return(
     <section className="mt-20 text-justify">
-    <p className="mt-10">As campanhas do <strong>Google Ads</strong> durante o período de 01/02/2023 a 28/02/2023 originaram <strong>747 cliques </strong>
-      (visitas ao site) e 9.598 impressões dos anúncios com um CTR de 7,78%, que é a proporção da frequência
-      com que as pessoas que visualizaram os anúncios clicaram nele</p>
-    <p className="mt-10">Foi investido <strong>R$663,96</strong> no período analisado. O custo por clique (CPC) foi de <strong>R$0,89</strong>.</p>
-    <p className="mt-10"><strong>Conversões: Cliques no botão do WhatsApp</strong></p>
-  </section>
+      <p className="mt-10">As campanhas do <strong>Google Ads</strong> durante o período de 01/02/2023 a 28/02/2023 originaram <strong>747 cliques </strong>
+        (visitas ao site) e 9.598 impressões dos anúncios com um CTR de 7,78%, que é a proporção da frequência
+        com que as pessoas que visualizaram os anúncios clicaram nele</p>
+      <p className="mt-10">Foi investido <strong>R$663,96</strong> no período analisado. O custo por clique (CPC) foi de <strong>R$0,89</strong>.</p>
+      <p className="mt-10"><strong>Conversões: Cliques no botão do WhatsApp</strong></p>
+    </section>
   )
 }
+
--- components/graph/index.jsx
+++ components/graph/index.jsx
@@ -1,3 +1,24 @@
-export function name(params) {
-  
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts'
+
+class Graph extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {},
+      series: [44, 55, 41, 17, 15],
+      labels: ['A', 'B', 'C', 'D', 'E']
+    }
+  }
+
+  render() {
+    return (
+      <Chart className="mt-10"
+       options={this.state.options} series={this.state.series} type="donut" width="320" />
+    );
+  }
 }
+
+export default Graph;
