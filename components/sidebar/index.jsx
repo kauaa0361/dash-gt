fe5963e6d394be57f3249aede947ca37efcde197
kauaa0361 <kauacosta1111@gmail.com>
2023-03-07 15:03:39 +0000

    create sidebar

---
+++ components/sidebar/index.jsx
@@ -0,0 +1,16 @@
+export function Sidebar() {
+  return(
+    <section className="absolute bg-black w-20 h-80 rounded-xl ml-2 mt-10">
+      <Teste/>
+      <Teste/>
+      <Teste/>
+      <Teste/>
+    </section>
+  )
+}
+
+function Teste() {
+  return (
+    <div className="py-12 bg-sky-400">a</div>
+  )
+}
